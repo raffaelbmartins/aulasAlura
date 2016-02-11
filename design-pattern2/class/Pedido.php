edcca61c9ce9b6b4f9a6d8871d7e1756af84b074
= <=>
2016-02-11 15:43:45 -0300

    Design Pattern 2 - Aula 7 Exerc. Classe Pedido

---
+++ design-pattern2/class/Pedido.php
@@ -0,0 +1,40 @@
+<?php
+
+class Pedido {
+    private $cliente;
+    private $valor;
+    private $status;
+    private $dataFinaliza;
+    
+    
+    function __construct($cliente, $valor, $status){
+        $this->cliente = $cliente;
+        $this->valor = $valor;
+        $this->status = $status;
+    }
+    
+    public function getCliente(){
+        return $this->cliente;
+    }
+    
+    public function getValor(){
+        return $this->valor;
+    }
+    
+    public function getStatus(){
+        return $this->status;
+    }
+    
+    public function getDataFinaliza(){
+        return $this->dataFinaliza;
+    }
+    
+    public function paga() {
+        $this->status = new Pago();
+    }
+    
+    public function finaliza() {
+        $this->dataFinalizacao = date("m/d/Y");
+        $this->status = new Entregue();
+    }
+}
---
+++ design-pattern2/pedidos/index.php
@@ -0,0 +1,4 @@
+<?php
+
+require '../AutoLoader.php';
+

037d33e94d7af81d2d30a24885c229a0a392e94f
= <=>
2016-02-12 12:14:23 -0300

    SOLID - Aula 1 Mao na Massa

--- solid/index.php
+++ solid/index.php
@@ -1,2 +1,15 @@
 <?php
 
+require 'AutoLoader.php';
+
+$cargo = new Desenvolvedor(new DezOuVintePorCento());
+
+$funcionario = new Funcionario($cargo,2000);
+$funcionario->setId(1);
+$funcionario->setNome("Raffael");
+$funcionario->setDataAdmisao(new DateTime("now"));
+
+$calculadora = new CalculadoraDeSalario();
+
+echo $calculadora->calcula($funcionario);
+
